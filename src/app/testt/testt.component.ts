f58f0e1c8848813034614038021b185f1fc36a39
fahad5359 <fabbgm5359@gmail.com>
2022-04-23 15:37:12 +0300

    yet more Property binding and string introplation testing

--- src/app/testt/testt.component.ts
+++ src/app/testt/testt.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-testt',
   templateUrl: './testt.component.html',
   styleUrls: ['./testt.component.css']
 })
-export class TesttComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class TesttComponent{
+  // as u can see we can property bind evrything, and change it values from here.
+  vaaalue="fahad";
+  nems=["kaled","maoth"]
+  vaaalue2="Ali";
+  btn="false";
+  inbty="time";
+   Changenems(){
+    this.vaaalue="ahmad"
+    // this.nems[0]="kaled is dead"
   }
+  sj(){
+    // we can even change the value of the input by addding an event to a button
+    this.inbty="number"
+  }
+
+   
+  
 
 }
