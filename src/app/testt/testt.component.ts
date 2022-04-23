aa8d01e665fa79de31a5f25d18f20a0b10358979
fahad5359 <fabbgm5359@gmail.com>
2022-04-23 17:32:04 +0300

    .

--- src/app/testt/testt.component.ts
+++ src/app/testt/testt.component.ts
@@ -7,21 +7,32 @@ import { Component} from '@angular/core';
 })
 export class TesttComponent{
   // as u can see we can property bind evrything, and change it values from here.
-  vaaalue="fahad";
-  nems=["kaled","maoth"]
-  vaaalue2="Ali";
-  btn="false";
-  inbty="time";
-   Changenems(){
-    this.vaaalue="ahmad"
+  // vaaalue="fahadddd";
+  // nems=["kaled","maoth"]
+  // vaaalue2="Ali";
+  // btn="false";
+  // inbty="time";
+  //  Changenems(){
+    // this.vaaalue="ahmad"
     // this.nems[0]="kaled is dead"
-  }
-  sj(){
+  // }
+  // sj(){
     // we can even change the value of the input by addding an event to a button
-    this.inbty="number"
-  }
+  //   this.inbty="number"
+  // }
+  // --------------------------------------------------------------
+  // <!-- so we want to display the input coming from the user.  -->
 
-   
+  giveNem=""
+  statuss=""
+
+  displayNem(event:any){
+    // console.log(event);
+    this.giveNem=event.target.value
+  }
+  bbtn(){
+    this.statuss=" Ur new nem is"+" "+this.giveNem
+  }
   
 
 }
