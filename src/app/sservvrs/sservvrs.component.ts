bfd530292211ad97d6c4910bd66b1cc3e9f5e0fb
fahad5359 <fabbgm5359@gmail.com>
2022-04-26 21:04:53 +0300

    fnshed HW learned more about the DDocumint object

--- src/app/sservvrs/sservvrs.component.ts
+++ src/app/sservvrs/sservvrs.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sservvrs',
   templateUrl: './sservvrs.component.html',
   styleUrls: ['./sservvrs.component.css']
 })
-export class SservvrsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SservvrsComponent{
+  @ViewChild("slct") slct: ElementRef;
+ 
+  showValue(){
+    console.log( this.slct.nativeElement.innerHTML);
   }
-
 }
